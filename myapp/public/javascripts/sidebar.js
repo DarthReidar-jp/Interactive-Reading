df6e9c490b2ed6397b0a7c39828f81bfc2642738
reira-endo <103876343+reira-endo@users.noreply.github.com>
2024-02-20 06:13:34 +0000

    arranded the design
    reader&writer

--- myapp/public/javascripts/sidebar.js
+++ myapp/public/javascripts/sidebar.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
     $('.hamburger-btn').on('click', function() {
-      $('body').toggleClass('sidebar-active');
+        // サイドバーが表示されているかチェック
+        if ($('body').hasClass('mode-active')) {
+            // モードコンテンツを非表示にする
+            $('body').removeClass('mode-active');
+        }
+        // サイドバーの表示をトグルする
+        $('body').toggleClass('sidebar-active');
     });
-  });
-  
+
+    $('.mode-btn').on('click', function() {
+        // サイドバーが表示されているかチェック
+        if ($('body').hasClass('sidebar-active')) {
+            // サイドバーを非表示にする
+            $('body').removeClass('sidebar-active');
+        }
+        // モードコンテンツの表示をトグルする
+        $('body').toggleClass('mode-active');
+    });
+});
