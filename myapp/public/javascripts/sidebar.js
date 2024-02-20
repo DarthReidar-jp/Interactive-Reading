2b604217fdfe48d67ddbd8290231f7efda4b263c
reira-endo <103876343+reira-endo@users.noreply.github.com>
2024-02-20 04:21:52 +0000

    add sidebar animation

---
+++ myapp/public/javascripts/sidebar.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.hamburger-btn').on('click', function() {
+      $('body').toggleClass('sidebar-active');
+    });
+  });
+  
--- myapp/src/routes/reader.ts
+++ myapp/src/routes/reader.ts
@@ -8,13 +8,13 @@ const router = express.Router();
 router.get('/:bookId', async (req: Request, res: Response) => {
     try {
         const { bookId } = req.params;
+
         // パラメータから取得したbookIdを使用してMongoDBのObjectIdを生成
         const objectId = new ObjectId(bookId);
         const collection = await getDBCollection('books');
 
         // 指定されたIDを持つBookをデータベースから検索
         const book = await collection.findOne({ _id: objectId });
-
         if (book) {
             // Bookが見つかった場合、readerをレンダリング
             res.render('reader', { book });
@@ -31,5 +31,30 @@ router.get('/:bookId', async (req: Request, res: Response) => {
     }
 });
 
+router.post('/memo', async (req: Request, res: Response) => {
+    try {
+        const { bookId } = req.params;
+
+        // パラメータから取得したbookIdを使用してMongoDBのObjectIdを生成
+        const objectId = new ObjectId(bookId);
+        const collection = await getDBCollection('books');
+
+        // 指定されたIDを持つBookをデータベースから検索
+        const book = await collection.findOne({ _id: objectId });
+        if (book) {
+            // Bookが見つかった場合、readerをレンダリング
+            res.render('reader', { book });
+        } else {
+            // Bookが見つからない場合、404エラーを表示
+            res.status(404).send('Book not found');
+        }
+    } catch (e) {
+        if (e instanceof Error) {
+            res.status(500).send(e.message);
+        } else {
+            res.status(500).send(String(e));
+        }
+    }
+});
 
 export default router;
